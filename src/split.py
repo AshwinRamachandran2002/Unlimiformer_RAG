473bd39d08b37e3bc13a42209c1e053bf98cb368
AshwinRamachandran2002 <200050016@iitb.ac.in>
2023-10-21 22:41:03 +0530

    added retrieval setting - token get fact take

---
+++ src/split.py
@@ -0,0 +1,35 @@
+import torch
+from transformers import AutoTokenizer
+
+model_name = "meta-llama/Llama-2-13b-chat-hf"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+data_file = "data14"
+with open(data_file + "/original_data.txt", 'r') as file:
+    file_contents = file.read()
+
+inputs = tokenizer.encode(file_contents)
+for i in range(len(inputs)):
+    print(tokenizer.decode(inputs[i]), end=" ")
+print(len(inputs))
+
+segments = file_contents.split(';')
+len_list = []
+for segment in segments:
+    inputs = tokenizer.encode(segment.strip())
+    for i in range(len(inputs)):
+        print(tokenizer.decode(inputs[i]), end=" ")
+    print(len(inputs))
+    len_list.append(len(inputs))
+tot_len = 0
+len_list[5] = len_list[5] - 1
+print(len_list)
+for i in range(len(len_list)):
+    tot_len += len_list[i]
+print(tot_len)
+config = {
+    "segment_length": len_list
+}
+import json 
+with open(data_file +"/config_data.json", "w") as f:
+    json.dump(config, f)
